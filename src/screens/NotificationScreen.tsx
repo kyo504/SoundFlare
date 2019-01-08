e67ba49c56d53c8a7e86cc11d99c17f3ecc637ba
Daniel Jung <daniel.jung@radishfiction.com>
2019-01-08 11:22:35 +0900

    Add prettier and commit hook

--- src/screens/NotificationScreen.tsx
+++ src/screens/NotificationScreen.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { NavigationScreenProps } from 'react-navigation';
+import React from "react";
+import { View, Text } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { NavigationScreenProps } from "react-navigation";
 
-export default class NotificationScreen extends React.Component<NavigationScreenProps> {
+export default class NotificationScreen extends React.Component<
+  NavigationScreenProps
+> {
   static navigationOptions = {
-    headerTitle: 'Notification',
-    tabBarIcon: ({ tintColor, focused }) => (
-      <Icon name={focused ? 'bell' : 'bell-outline'} size={26} style={{ color: tintColor }} />
-    ),
+    headerTitle: "Notification",
+    tabBarIcon: ({
+      tintColor,
+      focused
+    }: {
+      tintColor: string;
+      focused: boolean;
+      horizontal: boolean;
+    }): JSX.Element => (
+      <Icon
+        name={focused ? "bell" : "bell-outline"}
+        size={26}
+        style={{ color: tintColor }}
+      />
+    )
   };
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>NotificationScreen</Text>
         <Text
           onPress={() => {
